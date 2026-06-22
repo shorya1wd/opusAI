8217b1bf1664ccfec779b42df8402b1b3378ae74
shorya1wd <shoryabhushan0@gmail.com>
2026-06-22 19:00:21 +0530

    setup docker

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  
+  output: "standalone",
   compiler: {
     removeConsole: {
       exclude: ['error'],
